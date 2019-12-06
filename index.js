e650176fa7a044c33b209f507deeb441470e6fdb
Fouad <fouad.omri@predapp.com>
2019-12-06 23:24:24 +0100

    hello rayen

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  res.end('Hello Rayen\n')
 });
 
 server.listen(port, () => {
